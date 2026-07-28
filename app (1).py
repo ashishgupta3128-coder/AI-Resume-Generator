80b29ce427f5c6dcdac7da7317f496347f877eee
ashishgupta3128-coder <ashishgupta3128@gmail.com>
2026-07-28 12:22:44 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -24,7 +24,7 @@ from langchain.messages import SystemMessage, HumanMessage
 import numpy as np
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
-
+from PIL import Image
 
 #=================API KEY LOAD=========================
 
@@ -32,6 +32,11 @@ GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY",type="password")
 GROQ_API_KEY = st.sidebar.text_input("GROQ_API_KEY",type="password")
 TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY",type="password")
 
+if not (GOOGLE_API_KEY) and not (GROQ_API_KEY) and not (TAVILY_API_KEY):
+    st.sidebar.warning("PASS API KEYS")
+    st.stop()
+else:
+    st.success("API KEYS LOADED")
 
 #============================MODEL BUILDING========================
 model = ChatGoogleGenerativeAI(
@@ -97,6 +102,32 @@ def resume_maker_prompt():
   return prompt
 
 resume_maker_prompt()
+
+#================UPLOAD IMAGE=================
+uploaded_file = st.sidebar.file_uploader(
+    "choose an image file",
+    type=["jpg", "jpeg", "png", "webp"]
+)
+if uploaded_file is not None:
+    try:
+        image = Image.open(uploaded_file)
+
+        st.sidebar.image(image, caption="Uploaded Image", use_container_width=True)
+
+        if image.mode in ("RGBA", "P"):
+            image = image.convert("RGB")
+        base_name = os.path.splitext(uploaded_file.name)[0]
+        save_path = f"{base_name}.jpg"
+
+        # 3. Save the image to the current working directory
+
+        image.save(save_path, "JPEG")
+        st.sidebar.success(f"Image successfully saved as '(save_path)'!")
+
+     except Exception as e:
+         st.error(f"Error processing image: {e}")
+          
+        
 #===========================GENERATE RESUME===========
 prompt = """You are a helpful AI assistant
 with job resume maker, your task is to give
@@ -105,6 +136,13 @@ code, with professional design Format.
 user will upload data and return HTML format resume."""
 
 final_prompt = prompt + resume_maker_prompt()
+
+user_info = st.text_input("Enter your information")
+
+user_detail = f"""user details: given below:
+Resume info: {user_info}
+Photo: {uploaded_file}
+Default if not given: Give Python Developer Resume"""
 
 user_details = """Give Python Developer Resume.
 always use different color or styling
